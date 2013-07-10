89f8e581c5f488017f15ea8333b73bd8af3de0ee
Thea P <aapepperl@vcu.edu>
2013-07-10 20:48:36 +0000

    Played with buffer

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var buffer = new Buffer(8);
+buffer.write("Hello", "utf-8");
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
