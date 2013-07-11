3e57c30aa3b6c521afd8b3341c8deed6b2bf5ddd
Thea P <aapepperl@vcu.edu>
2013-07-11 04:00:39 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,11 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 var buffer = new Buffer(8);
 buffer.write("Hello", "utf-8");
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString("utf-8",0,12);
+  response.send('Hello');
 });
 
 var port = process.env.PORT || 5000;
