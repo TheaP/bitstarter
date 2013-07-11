82fd8953c357a931ae8727dafc93a2ab45a5794f
Thea P <aapepperl@vcu.edu>
2013-07-11 04:35:01 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var fs = require('fs');
 
+fs.readFile('./index.html', function(err,data){
+    if (err){
+	throw err;
+    }
+    var buffer = new Buffer(24);
+    buffer.write(data, "utf-8");
+});
+
 var app = express.createServer(express.logger());
-var buffer = new Buffer(8);
-buffer.write("Hello", "utf-8");
+console.log(buffer.toString('utf-8'));
 
 app.get('/', function(request, response) {
-  response.send('Hello');
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
